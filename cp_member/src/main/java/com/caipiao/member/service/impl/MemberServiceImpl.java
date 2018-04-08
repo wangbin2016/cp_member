60fc1827e23af2ec1a61f9cbb61bd82bb3cbe21f
WangB <wxjwb@qq.com>
2018-04-08 16:22:31 +0800

    add md5

--- cp_member/src/main/java/com/caipiao/member/service/impl/MemberServiceImpl.java
+++ cp_member/src/main/java/com/caipiao/member/service/impl/MemberServiceImpl.java
@@ -3,6 +3,7 @@ package com.caipiao.member.service.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.caipiao.common.utils.MD5;
 import com.caipiao.member.dao.MemberMapper;
 import com.caipiao.member.entity.Member;
 import com.caipiao.member.service.MemberService;
@@ -15,16 +16,19 @@ public class MemberServiceImpl implements MemberService{
 	
 	@Override
 	public void addMember(Member member) {
+		member.setPassword(MD5.sign(member.getPassword()));
 		memberMapper.insert(member);
 	}
 	
 	@Override
 	public void regist(Member member) {
+		member.setPassword(MD5.sign(member.getPassword()));
 		memberMapper.insert(member);
 	}
 
 	@Override
 	public Member getMember(String account, String password) {
+		password = MD5.sign(password);
 		return memberMapper.findMemberByLogin(account, password);
 	}
 
